9d241d38110cfb67253ca29a053049f07c897408
Shabbeer Petersen <Shabbeer.Petersen@capaciti.org.za>
2024-10-07 11:23:50 +0200

    practicing

--- hello.py
+++ hello.py
@@ -1,53 +1,97 @@
-print("Hello World!")
+# print("Hello World!")
 
-# Basic data types
-name = "Alice"  # string
-age = 25    # integer
-height = 5.9    # float
-isStudent = True    # boolean
+# # Basic data types
+# name = "Alice"  # string
+# age = 25    # integer
+# height = 5.9    # float
+# isStudent = True    # boolean
 
-# Compound data types
+# # Compound data types
 
-fruits = ['apple','banana','cherry']    # list
-coordinates = (10.0, 20.0)  # tuple
-person = {'name':'Alice',   # dictionary
-          'age': 25}
-unique_numbers = {1,2,3,4,5}     #set
+# fruits = ['apple','banana','cherry']    # list
+# coordinates = (10.0, 20.0)  # tuple
+# person = {'name':'Alice',   # dictionary
+#           'age': 25}
+# unique_numbers = {1,2,3,4,5}     #set
 
-result = 10 + 3.5 # result will be a float
+# result = 10 + 3.5 # result will be a float
 
-num_str = '100'
-num_int = int(num_str) # converts to string
+# num_str = '100'
+# num_int = int(num_str) # converts to string
 
-# Conditional statements --------------------------------
+# # Conditional statements --------------------------------
 
-#If-Else
+# #If-Else
 
-x = 10
-if x > 0:
-    print('x is positive')
-elif x == 0:
-    print('x is zero')
-else:
-    print('x is negative')
+# x = 10
+# if x > 0:
+#     print('x is positive')
+# elif x == 0:
+#     print('x is zero')
+# else:
+#     print('x is negative')
 
-# For loop
-for i in range(5):
-    print(i)
+# # For loop
+# for i in range(5):
+#     print(i)
 
-# While loop
-count = 0
-while count < 5:
-    print(count)
-    count += 1
+# # While loop
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
     
-# Combining If-Else with Loops
+# # Combining If-Else with Loops
 
-numbers = [1,2,3,4,5]
+# numbers = [1,2,3,4,5]
 
-for num in numbers:
-    if num % 2 == 0:
-        print(f'{num} is even')
-    else:
-        print(f'{num} is odd')
+# for num in numbers:
+#     if num % 2 == 0:
+#         print(f'{num} is even')
+#     else:
+#         print(f'{num} is odd')
         
+# Functions -------------------------------------------------------
+
+def greet(name):
+    return f'hello, {name}!'
+
+print(greet("Shabbeer"))
+
+# Modules ------------------------------------------------------
+
+#  creating and importing and modules
+import math_module
+ 
+add_result = math_module.add(5,3)
+subtract_result = math_module.subtract(5,3)
+multiply_result = math_module.multiply(5,3)
+divide_result = math_module.divide(5,3)
+
+print(add_result,subtract_result,multiply_result,divide_result)
+
+# Existing module
+
+import math
+#print(math.sqrt(16))
+
+# Data Structure Manipulation -----------------------------------------
+
+# list manipulation
+fruits = ['apple','banana','cherry']
+fruits.append('orange')
+
+# tuple cannot be changed but can be displayed
+coordinates = (10.0,20.0)
+x,y = coordinates
+
+# dictionary manipulation
+
+person = {'name': "Shabbeer",
+          'age': 25}
+person['age'] = 30
+
+# sets manipulation
+unique_numbers ={1,2,3,4}
+unique_numbers.add(5)
+
